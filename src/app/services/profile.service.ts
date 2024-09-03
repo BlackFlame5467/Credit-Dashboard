ba08890e55cae48d775403f9b37754c9a571f4b0
BlackFlame5467 <BlackFlame5467@gmail.com>
2024-09-03 19:46:42 +0300

    v1.1

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,62 +1,72 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { map, Observable, switchMap } from 'rxjs';
-import { IPaginationProfile, IProfile } from '../interfaces/profile.interface';
-import { CheckService } from './check.service';
+import { HttpClient } from '@angular/common/http'
+import { inject, Injectable, signal } from '@angular/core'
+import { map, Observable, of, shareReplay, switchMap, tap } from 'rxjs'
+import { IPaginationProfile, IProfile } from '../interfaces/profile.interface'
+import { CheckService } from './check.service'
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class ProfileService {
-  http = inject(HttpClient);
-  checkService = inject(CheckService);
-  currentDate = new Date().toISOString().split('T')[0];
-  pageSize: number = 10;
-  currentPage: number = 1;
+	http = inject(HttpClient)
+	checkService = inject(CheckService)
+	currentDate = new Date().toISOString().split('T')[0]
+	pageSize: number = 10
+	currentPage: number = 1
+	private store$!: Observable<IProfile[]>
 
-  getProfile(): Observable<IProfile[]> {
-    return this.http.get<IProfile[]>(
-      'https://raw.githubusercontent.com/LightOfTheSun/front-end-coding-task-db/master/db.json'
-    );
-  }
+	getProfile(): Observable<IProfile[]> {
+		if (this.store$) {
+			console.log('Кэш')
+			return this.store$
+		} else {
+			console.log('Сервер')
+			this.store$ = this.http
+				.get<IProfile[]>(
+					'https://raw.githubusercontent.com/LightOfTheSun/front-end-coding-task-db/master/db.json'
+				)
+				.pipe(shareReplay(1))
+			return this.store$
+		}
+	}
 
-  getFilterProfiles(page: number): Observable<IPaginationProfile> {
-    this.currentPage = page;
-    return this.getProfile().pipe(
-      switchMap((profiles) =>
-        this.checkService.isChecked$.pipe(
-          map((isChecked) => {
-            let filteredProfiles = isChecked
-              ? this.filterProfiles(profiles)
-              : profiles;
-            return {
-              profiles: this.paginationProfiles(
-                filteredProfiles,
-                this.currentPage
-              ),
-              totalCount: isChecked
-                ? this.filterProfiles(profiles).length
-                : profiles.length,
-              isChecked: isChecked,
-            };
-          })
-        )
-      )
-    );
-  }
+	getFilterProfiles(page: number): Observable<IPaginationProfile> {
+		this.currentPage = page
+		return this.getProfile().pipe(
+			switchMap(profiles =>
+				this.checkService.isChecked$.pipe(
+					map(isChecked => {
+						let filteredProfiles = isChecked
+							? this.filterProfiles(profiles)
+							: profiles
+						return {
+							profiles: this.paginationProfiles(
+								filteredProfiles,
+								this.currentPage
+							),
+							totalCount: isChecked
+								? this.filterProfiles(profiles).length
+								: profiles.length,
+							isChecked: isChecked,
+						}
+					})
+				)
+			)
+		)
+	}
 
-  filterProfiles(profiles: IProfile[]): IProfile[] {
-    return profiles.filter((profile) => {
-      return (
-        profile.actual_return_date > profile.return_date ||
-        (profile.return_date < this.currentDate && !profile.actual_return_date)
-      );
-    });
-  }
+	filterProfiles(profiles: IProfile[]): IProfile[] {
+		return profiles.filter(profile => {
+			return (
+				profile.actual_return_date > profile.return_date ||
+				(profile.return_date < this.currentDate && !profile.actual_return_date)
+			)
+		})
+	}
 
-  paginationProfiles(profiles: IProfile[], currentPage: number): IProfile[] {
-    const start = (currentPage - 1) * this.pageSize;
-    const end = start + this.pageSize;
-    return profiles.slice(start, end);
-  }
+	paginationProfiles(profiles: IProfile[], currentPage: number): IProfile[] {
+		const start = (currentPage - 1) * this.pageSize
+		const end = start + this.pageSize
+		return profiles.slice(start, end)
+	}
 }
